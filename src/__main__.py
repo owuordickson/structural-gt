db85a1a4ba4b140d99eb4ede3eb0080753d8c0ef
owuordickson <owuordickson@gmail.com>
2024-01-04 19:37:11 -0500

    update: splash page

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,113 @@
 """
-ChaosGT
+**ChaosGT**
 
 A python package for chaos engineering in nano-structures.
+
+Copyright (C) 2024, The University of Michigan.
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Development Lead: Dickson Owuor
+Contributors: Nicholas A. Kotov
+Contact email: owuordickson@gmail.com
 """
 
 __author__ = "Dickson Owuor"
 __credits__ = "Chemical Engineering Department, University of Michigan"
 
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+
+class MainFrame(ttk.Frame):
+
+    def __init__(self, container):
+        super().__init__(container)
+
+        # set up grid layout
+        self.columnconfigure(0, weight=2)
+        self.columnconfigure(1, weight=3)
+
+        # initialize widgets
+        self.__create_widgets()
+
+    def __create_widgets(self):
+        # (button) About
+        self.btn_about = ttk.Button(self, text='About')
+        self.btn_about['command'] = self.btn_about_clicked
+        self.btn_about.grid(column=1, row=0, padx=5, pady=5, sticky=tk.E)
+
+        # (button) Select Image
+        self.btn_select_img = ttk.Button(self, text='Select Image')
+        self.btn_select_img['command'] = self.btn_select_img_clicked
+        self.btn_select_img.grid(column=0, row=1, padx=5, pady=2, sticky=tk.EW)
+
+        # (button) ChaosGT Image
+        self.btn_chaosgt = ttk.Button(self, text='Proceed to Chaos GT')
+        self.btn_chaosgt['command'] = self.btn_chaosgt_clicked
+        self.btn_chaosgt.grid(column=0, row=2, padx=5, pady=2, sticky=tk.NW)
+        # self.btn_chaosgt.state(['disabled'])
+        self.btn_chaosgt.grid_forget()
+
+        # Image Placeholder
+        self.cvs_image = tk.Canvas(self, width=300, height=300, bg='white')
+        self.cvs_image.grid(column=1, row=1, rowspan=12, padx=5, pady=2, sticky=tk.EW)
+
+        #for widget in self.winfo_children():
+        #    widget.grid(padx=5, pady=2)
+
+    def btn_about_clicked(self):
+        about_info = "Chaos-GT is an application for implemeting chaos engineering in nano-structures. "\
+                     "The application was designed by Dickson Owuor and Nicholas Kotov."\
+                     "\n\n\n" \
+                     "Copyright (C) 2024, University of Michigan."\
+                     "\n\n"
+        messagebox.showinfo(
+            title = 'About Chaos GT',
+            message = about_info
+        )
+
+    def btn_select_img_clicked(self):
+        self.btn_chaosgt.grid(column=0, row=2, sticky=tk.NW)
+        # self.btn_chaosgt.state(['!disabled'])
+        pass
+
+    def btn_chaosgt_clicked(self):
+        pass
+
+
+
+class ChaosApp(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+
+        # configure the root window
+        self.title('Chaos Engineering in NP')
+        self.geometry('640x360')
+        self.resizable(0, 0)
+
+        # layout for root window
+        self.columnconfigure(0, weight=1)
+
+        main_frame = MainFrame(self)
+        main_frame.grid(column=0, row=0)
+
+
+if __name__ == "__main__":
+
+    app = ChaosApp()
+    # frame = MainFrame(app)
+    app.mainloop()
