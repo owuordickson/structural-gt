85f8a4d47e6cb04a87a115a1e8be99e71f1fb8c0
owuordickson <owuordickson@gmail.com>
2024-01-09 15:23:22 -0500

    updated

--- src/chaosgt/_gui_windows.py
+++ src/chaosgt/_gui_windows.py
@@ -11,13 +11,13 @@ import os
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from PIL import Image, ImageTk
-from .chaosgt import load_img_from_file, resize_img
+from .chaosgt import ChaoticStruct
 
 
 class LaunchWindow(ttk.Frame):
 
-    def __init__(self, container):
-        super().__init__(container)
+    def __init__(self, parent):
+        super().__init__(parent)
 
         # about information
         self.about_info = "Chaos-GT is an application for implementing chaos engineering in nano-structures. " \
@@ -26,9 +26,10 @@ class LaunchWindow(ttk.Frame):
                           "Copyright (C) 2024, University of Michigan." \
                           "\n\n"
 
-        # variables to store path and dir
+        # variables and objects
+        self.parent = parent
+        self.chaos_obj = None
         self.error_msg = ''
-        self.orig_img = None
 
         # set up grid layout
         self.columnconfigure(0, weight=2)
@@ -98,7 +99,8 @@ class LaunchWindow(ttk.Frame):
             save_dir, file_name = os.path.split(fd_image_file)
 
             # load image from file and resize it to 300 pixel
-            std_img = resize_img(load_img_from_file(os.path.join(save_dir, file_name)), 300)
+            self.chaos_obj = ChaoticStruct(os.path.join(save_dir, file_name))
+            std_img = self.chaos_obj.resize_img(300)
 
             # convert the Image object into a TkPhoto object
             img = Image.fromarray(std_img)
@@ -127,7 +129,53 @@ class LaunchWindow(ttk.Frame):
             self.lbl_msg['text'] = self.error_msg
 
     def __btn_proceed_clicked(self):
-        pass
+
+        # re-configure the root window
+        self.parent.title('Nano Structure Analysis')
+        self.parent.geometry('800x600')
+
+        # initialize and load window for analysis
+        analytic_window = AnalysisWindow(self.parent, self.chaos_obj)
+        analytic_window.grid(column=0, row=0)
+
+
+class AnalysisWindow(ttk.Frame):
+
+    def __init__(self, parent, c_obj):
+        super().__init__(parent)
+
+        # variables
+        self.parent = parent
+        self.error_msg = ''
+        self.chaos_obj = c_obj
+
+        # set up grid layout
+        self.columnconfigure(0, weight=2)
+        self.columnconfigure(1, weight=1)
+
+        # initialize widgets
+        self.__create_widgets()
+
+    def __create_widgets(self):
+
+        # convert the Image object into a TkPhoto object
+        std_img = self.chaos_obj.resize_img(512)
+        img = Image.fromarray(std_img)
+        img_tk = ImageTk.PhotoImage(image=img)
+
+        # Titles
+        self.lbl_title_res = tk.Label(self, text="Visualize Results", font="Bold")
+        self.lbl_title_res.grid(column=0, row=0, padx=2, pady=2, sticky=tk.W)
+        self.lbl_title_set = tk.Label(self, text="Modify Settings", font="Bold")
+        self.lbl_title_set.grid(column=1, row=0, padx=50, pady=2, sticky=tk.W)
+
+        # Image
+        self.lbl_image = tk.Label(self, image=img_tk)
+        self.lbl_image.image = img_tk
+        self.lbl_image.grid(column=0, row=1, rowspan=4, padx=2, pady=2, sticky=tk.NW)
+
+        self.lbl_text = tk.Label(self, text="start analysis")
+        self.lbl_text.grid(column=1, row=1, padx=2, pady=2, sticky=tk.EW)
 
 
 class ChaosGUI(tk.Tk):
--- src/chaosgt/chaosgt.py
+++ src/chaosgt/chaosgt.py
@@ -10,19 +10,24 @@ Processing of images and chaos engineering
 import cv2
 
 
-def load_img_from_file(file_path):
-    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
-    return img
+class ChaoticStruct:
 
+    def __init__(self, img_path):
+        self.img = ChaoticStruct.load_img_from_file(img_path)
 
-def resize_img(img, size):
-    w, h = img.shape
-    if h > w:
-        scale_factor = size / h
-    else:
-        scale_factor = size / w
-    std_width = int(scale_factor * w)
-    std_height = int(scale_factor * h)
-    std_size = (std_height, std_width)
-    std_img = cv2.resize(img, std_size)
-    return std_img
+    def resize_img(self, size):
+        w, h = self.img.shape
+        if h > w:
+            scale_factor = size / h
+        else:
+            scale_factor = size / w
+        std_width = int(scale_factor * w)
+        std_height = int(scale_factor * h)
+        std_size = (std_height, std_width)
+        std_img = cv2.resize(self.img, std_size)
+        return std_img
+
+    @staticmethod
+    def load_img_from_file(file):
+        img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
+        return img
