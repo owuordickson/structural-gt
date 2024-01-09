51a35e00f5402d371089c232e44fd621df011af4
owuordickson <owuordickson@gmail.com>
2024-01-09 11:25:51 -0500

    updated

--- src/chaosgt/_gui_windows.py
+++ src/chaosgt/_gui_windows.py
@@ -7,17 +7,24 @@
 Tk-inter window implementations
 """
 
-import cv2
 import os
 import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
 from PIL import Image, ImageTk
+from .chaosgt import load_img_from_file, resize_img
 
 
-class MainFrame(ttk.Frame):
+class LaunchWindow(ttk.Frame):
 
     def __init__(self, container):
         super().__init__(container)
+
+        # about information
+        self.about_info = "Chaos-GT is an application for implementing chaos engineering in nano-structures. " \
+                          "The application was designed by Dickson Owuor and Nicholas Kotov." \
+                          "\n\n\n" \
+                          "Copyright (C) 2024, University of Michigan." \
+                          "\n\n"
 
         # variables to store path and dir
         self.error_msg = ''
@@ -34,21 +41,21 @@ class MainFrame(ttk.Frame):
 
         # (button) About
         self.btn_about = ttk.Button(self, text='About')
-        self.btn_about['command'] = self.btn_about_clicked
+        self.btn_about['command'] = self.__btn_about_clicked
         self.btn_about.grid(column=1, row=0, padx=5, pady=5, sticky=tk.E)
 
         # (button) Select Image File
         self.btn_select_img = tk.Button(self, text='Select Image', width=15, height=2, compound='c')
         self.btn_select_img['fg'] = 'dark green'
-        self.btn_select_img['command'] = self.btn_select_img_clicked
+        self.btn_select_img['command'] = self.__btn_select_img_clicked
         self.btn_select_img.grid(column=0, row=1, padx=5, pady=2, sticky=tk.EW)
 
         # (button) ChaosGT Algorithm
-        self.btn_chaosgt = tk.Button(self, text='Proceed to Chaos GT', width=15, height=2, compound='c')
-        self.btn_chaosgt['fg'] = 'dark green'
-        self.btn_chaosgt['command'] = self.btn_chaosgt_clicked
-        self.btn_chaosgt['state'] = 'disabled'
-        self.btn_chaosgt.grid(column=0, row=2, padx=5, pady=2, sticky=tk.NW)
+        self.btn_proceed = tk.Button(self, text='Proceed to Chaos GT', width=15, height=2, compound='c')
+        self.btn_proceed['fg'] = 'dark green'
+        self.btn_proceed['command'] = self.__btn_proceed_clicked
+        self.btn_proceed['state'] = 'disabled'
+        self.btn_proceed.grid(column=0, row=2, padx=5, pady=2, sticky=tk.NW)
 
         # (label) Show status/error
         self.pgb_status = ttk.Progressbar(self, mode='indeterminate')
@@ -64,22 +71,17 @@ class MainFrame(ttk.Frame):
         self.lbl_image = tk.Label(self, image=pixel, width=300, height=300, bg='white')
         self.lbl_image.grid(column=1, row=1, rowspan=12, padx=5, pady=2, sticky=tk.EW)
 
-    def btn_about_clicked(self):
-        about_info = "Chaos-GT is an application for implemeting chaos engineering in nano-structures. "\
-                     "The application was designed by Dickson Owuor and Nicholas Kotov."\
-                     "\n\n\n" \
-                     "Copyright (C) 2024, University of Michigan."\
-                     "\n\n"
+    def __btn_about_clicked(self):
         messagebox.showinfo(
-            title = 'About Chaos GT',
-            message = about_info
+            title='About Chaos GT',
+            message=self.about_info
         )
 
-    def btn_select_img_clicked(self):
+    def __btn_select_img_clicked(self):
 
         # disable btn_select until image is loaded
         self.btn_select_img['state'] = 'disabled'
-        self.btn_chaosgt['state'] = 'disabled'
+        self.btn_proceed['state'] = 'disabled'
         self.pgb_status.grid(column=0, row=4, padx=5, pady=1, sticky=tk.SW)
         self.lbl_msg.grid(column=0, row=5, padx=5, pady=1, sticky=tk.NW)
 
@@ -95,11 +97,8 @@ class MainFrame(ttk.Frame):
             # split the file location into file name and path
             save_dir, file_name = os.path.split(fd_image_file)
 
-            # read and load image from file
-            self.orig_img = cv2.imread(os.path.join(save_dir, file_name), cv2.IMREAD_GRAYSCALE)
-
-            # resize image to 300 pixel
-            std_img = self.resize_img(300)
+            # load image from file and resize it to 300 pixel
+            std_img = resize_img(load_img_from_file(os.path.join(save_dir, file_name), 300))
 
             # convert the Image object into a TkPhoto object
             img = Image.fromarray(std_img)
@@ -113,7 +112,7 @@ class MainFrame(ttk.Frame):
 
             # enable ChaosGT button and Select button
             self.btn_select_img['state'] = 'normal'
-            self.btn_chaosgt['state'] = 'normal'
+            self.btn_proceed['state'] = 'normal'
             self.pgb_status.stop()
             self.pgb_status.grid_forget()
             self.lbl_msg['text'] = 'Ready for chaos\nengineering!'
@@ -122,25 +121,13 @@ class MainFrame(ttk.Frame):
 
             # disable ChaosGT button
             self.btn_select_img['state'] = 'normal'
-            self.btn_chaosgt['state'] = 'disabled'
+            self.btn_proceed['state'] = 'disabled'
             self.pgb_status.stop()
             self.pgb_status.grid_forget()
             self.lbl_msg['text'] = self.error_msg
 
-    def btn_chaosgt_clicked(self):
+    def __btn_proceed_clicked(self):
         pass
-
-    def resize_img(self, size):
-        w, h = self.orig_img.shape
-        if (h > w):
-            scalefactor = size / h
-        else:
-            scalefactor = size / w
-        std_width = int(scalefactor * w)
-        std_height = int(scalefactor * h)
-        std_size = (std_height, std_width)
-        std_img = cv2.resize(self.orig_img, std_size)
-        return std_img
 
 
 class ChaosGUI(tk.Tk):
@@ -157,5 +144,5 @@ class ChaosGUI(tk.Tk):
         self.columnconfigure(0, weight=1)
 
         # load window components
-        main_frame = MainFrame(self)
-        main_frame.grid(column=0, row=0)
+        splash_frame = LaunchWindow(self)
+        splash_frame.grid(column=0, row=0)
--- src/chaosgt/chaosgt.py
+++ src/chaosgt/chaosgt.py
@@ -1,27 +1,28 @@
-# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: GNU GPL v3
+# This file is dual licensed under the terms of the GNU General Public, Version
+# 3.0.  See the LICENSE file in the root of this
+# repository for complete details.
+
 """
-**ChaosGT**
-
-A Python package for automated chaos engineering in nano-structures.
-
-Copyright (C) 2024, The University of Michigan.
-
-This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-
-
-Development Lead: Dickson Owuor
-Contributors: Nicholas A. Kotov
-Contact email: owuordickson@gmail.com
+Processing of images and chaos engineering
 """
+
+import cv2
+
+
+def load_img_from_file(file_path):
+    img = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE)
+    return img
+
+
+def resize_img(img, size):
+    w, h = img.shape
+    if h > w:
+        scale_factor = size / h
+    else:
+        scale_factor = size / w
+    std_width = int(scale_factor * w)
+    std_height = int(scale_factor * h)
+    std_size = (std_height, std_width)
+    std_img = cv2.resize(img, std_size)
+    return std_img
