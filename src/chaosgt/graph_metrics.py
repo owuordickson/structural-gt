0d67a6c01fd53539b3aeb248ae404ab049a16dfb
owuordickson <owuordickson@gmail.com>
2024-01-25 19:14:19 -0500

    added

---
+++ src/chaosgt/graph_metrics.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: GNU GPL v3
+# This file is dual licensed under the terms of the GNU General Public, Version
+# 3.0.  See the LICENSE file in the root of this
+# repository for complete details.
+
+"""
+Compute graph theory metrics
+"""
+
+
+class GraphMetrics:
+
+    def __init__(self, g_obj):
+        self.nx_graph = g_obj
+
+    def compute_gt_metrics(self):
+        pass
+
+    def compute_weighted_gt_metrics(self):
+        pass
