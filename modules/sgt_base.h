4a0f68f892935f17d1883fc984b625a2bcaf7d96
owuordickson <owuordickson@gmail.com>
2024-02-28 17:21:06 -0500

    updated

---
+++ modules/sgt_base.h
@@ -0,0 +1,4 @@
+
+typedef struct ThreadArgsLNC;
+void* compute_lnc(void *arg);
+int** str_to_matrix(char* str_adj_mat, int num_vertices);
--- modules/sgt_module.c
+++ modules/sgt_module.c
@@ -5,6 +5,7 @@
 
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
+#include "sgt_base.h"
 
 
 static PyObject *ErrorObject;
@@ -18,6 +19,7 @@ compute_anc(PyObject *self, PyObject *args)
     char *str_adj_mat;
 	igraph_t graph;
 	int size;
+	int** adj_mat
 
     if (!PyArg_ParseTuple(args, "siii:compute_anc", &str_adj_mat, &length, &num_cpus, &allow_mp))
         return NULL;
@@ -27,12 +29,33 @@ compute_anc(PyObject *self, PyObject *args)
     	return NULL;
   	}
 
+	// Get size of adjascency matrix
 	size = (int)sqrt((double)(*length));
+
+	// Convert string to matrix
+    adj_mat = str_to_matrix(str_adj_mat, size);
 
 	igraph_adjacency(
 		&graph, adj_mat, 
 		IGRAPH_ADJ_UNDIRECTED, IGRAPH_NO_LOOPS
 	);
+
+
+
+	// Print the matrix
+    printf("Adjacency Matrix:\n");
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", adj_mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < size; i++) {
+        free(adj_mat[i]);
+    }
+    free(adj_mat);
 
 }
 static char compute_anc_doc[] =
