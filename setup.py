367b41fd102e321f3a5bfc2f5fd5c581628d542a
owuordickson <owuordickson@gmail.com>
2024-02-28 23:08:00 -0500

    added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="sgt_module",
+            sources=["modules/sgt_base.c", "modules/sgt_modules.c"],
+            libraries=["igraph"],
+            library_dirs=["modules/igraph_lib/lib"],
+            include_dirs=["modules/igraph_lib/include/igraph"]
+        ),
+    ]
+)
