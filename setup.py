bd9ebcd2c39a11e3d0306fafe314dc74e090d592
owuordickson <owuordickson@gmail.com>
2024-03-01 15:19:52 -0500

    updated

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
         Extension(
             name="sgt",
             sources=["c_modules/sgtmodule.c", "c_modules/sgt_base.c"],
-            libraries=["igraph"],
-            # library_dirs=["c_modules/igraph_lib/lib"],
-            library_dirs=["/opt/homebrew/Cellar/igraph/0.10.10/lib"],
-            # extra_objects=["c_modules/igraph_lib/lib/libigraph.a"],
-            # include_dirs=["c_modules/igraph_lib/include/igraph"],
-            include_dirs=["/opt/homebrew/Cellar/igraph/0.10.10/include/igraph"],
-            # extra_compile_args=["-fdeclspec"]  # Add the compiler flag here
+            # libraries=["igraph"],  # macOS and Linux
+            # include_dirs=["/opt/homebrew/Cellar/igraph/0.10.10/include/igraph"],  # macOS and Linux
+            # library_dirs=["/opt/homebrew/Cellar/igraph/0.10.10/lib"],  # macOS and Linux
+            libraries=["igraph", "pthread"],  # Windows
+            include_dirs=["C:/MinGW/include/igraph", "C:/MinGW/include"],  # Windows
+            library_dirs=["C:/MinGW/lib"],  # Windows
+            # extra_compile_args=["-std=c99", "-fdeclspec"]  # Add the compiler flag here
         ),
     ]
 )
