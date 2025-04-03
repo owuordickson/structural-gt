a74ea5b2bee13dc166ae9e05a548b8f88cea92ac
owuordickson <owuordickson@gmail.com>
2025-04-02 23:31:46 -0400

    StructuralGT 3.2.5 - Alain's 3D implementation

--- src/StructuralGT/Networks/error.py
+++ src/StructuralGT/Networks/error.py
@@ -6,7 +6,7 @@ class ImageDirectoryError(ValueError):
     """Raised when a directory is accessed but does not have any images"""
 
     def __init__(self, directory_name):
-        self.directroy_name = directory_name
+        self.directory_name = directory_name
 
     def __str__(self):
         """Returns the error message"""
