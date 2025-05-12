4c3f7bbf88cf1b6447b45055b6ced2d5296b9a19
owuordickson <owuordickson@gmail.com>
2025-05-12 10:27:18 -0400

    StructuralGT v3.3.2

--- src/StructuralGT/__init__.py
+++ src/StructuralGT/__init__.py
@@ -29,7 +29,7 @@ of the GNU General Public License along with this program.  If not, see <https:/
 
 
 # Project Details
-__version__ = "3.3.1"
+__version__ = "3.3.2"
 __title__ = f"StructuralGT (v{__version__})"
 __author__ = "Dickson Owuor"
 __credits__ = "The Regents of the University of Michigan"
