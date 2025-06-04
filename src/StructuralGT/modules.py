0d38840a358bc85d9686d32c64eacb44748be603
owuordickson <owuordickson@gmail.com>
2025-06-04 13:33:10 -0400

    StructuralGT v3.3.4: scaling behavior (without boxplot)

--- src/StructuralGT/modules.py
+++ src/StructuralGT/modules.py
@@ -10,4 +10,20 @@ from .compute.graph_analyzer import GraphAnalyzer
 from .imaging.image_processor import ImageProcessor, ALLOWED_IMG_EXTENSIONS
 from .networks.fiber_network import FiberNetworkBuilder
 from .networks.graph_skeleton import GraphSkeleton
-from .utils.config_loader import load_gtc_configs, load_gte_configs, load_img_configs
+from .utils.config_loader import (
+    load_gtc_configs,
+    load_gte_configs,
+    load_img_configs
+)
+
+__all__ = [
+    "BaseImage",
+    "GraphAnalyzer",
+    "ImageProcessor",
+    "ALLOWED_IMG_EXTENSIONS",
+    "FiberNetworkBuilder",
+    "GraphSkeleton",
+    "load_gtc_configs",
+    "load_gte_configs",
+    "load_img_configs"
+]
