0b29a57f00d7e8479411ffe5a813622b51835cdc
owuordickson <owuordickson@gmail.com>
2025-06-27 11:28:14 -0400

    StructuralGT v3.3.9: giant graph (show button, use in GT compute)

--- src/StructuralGT/imaging/image_processor.py
+++ src/StructuralGT/imaging/image_processor.py
@@ -496,7 +496,7 @@ class ImageProcessor(ProgressUpdate):
                 success = graph_patch.extract_graph(img_patch, is_img_2d=True)
                 if success:
                     height, width = img_patch.shape
-                    graph_groups[(height, width)].append(graph_patch.nx_graph)
+                    graph_groups[(height, width)].append(graph_patch.nx_giant_graph)
                 else:
                     self.update_status([101, f"Filter {img_patch.shape} graph extraction failed!"])
 
