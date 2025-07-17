8c6d442c45889785e8fde737baec8c44bfa05035
owuordickson <owuordickson@gmail.com>
2025-07-17 14:56:03 -0400

    version 3.4.7: random patch bug

--- src/sgtlib/__init__.py
+++ src/sgtlib/__init__.py
@@ -24,7 +24,7 @@ of the GNU General Public License along with this program.  If not, see <https:/
 
 
 # Project Details
-__version__ = "3.4.6"
+__version__ = "3.4.7"
 __install_version__ = "3.4.4"
 __title__ = f"StructuralGT (v{__version__})"
 __author__ = "Dickson Owuor"
