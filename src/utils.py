bb1eda6b36e57c8181d2ada267e42d6fd77bc15d
owuordickson <owuordickson@gmail.com>
2025-09-16 17:59:44 -0400

    v1.0.0: cnn_model with pytorch; CNN pretrained models: resnet, densenet, ViT; automated graph generation-based on pure random filters; genetic algorithm environment; genetic algorithm for SGT

--- src/utils.py
+++ src/utils.py
@@ -393,19 +393,26 @@ def metaheuristic_image_configs(ntwk_obj: ImageProcessor, max_iters: int = 4, ga
             f"Graph Accuracy: {filter_space.best_candidate.graph_accuracy}\n"
             f"")
 
-    alg_options = {"apply_selections": 1, "filter_values": 1, "brightness_contrast": 1}
+    options_model = {
+        "find_filter_selections": {"id": "find_filter_selections", "type": "model-settings",
+                                   "text": "Find Best Image Filter Combination", "value": 1},
+        "find_filter_values": {"id": "find_filter_values", "type": "model-settings",
+                               "text": "Estimate Image Filter Values", "value": 1},
+        "find_brightness_contrast": {"id": "find_brightness_contrast", "type": "model-settings",
+                                     "text": "Estimate Brightness and Contrast Values", "value": 1},
+    }
 
     # 1. Create a search space
     filter_space = FilterSearchSpace.build_search_space(ntwk_obj.image_obj, initial_pop=ga_init_pop)
     _print_configs("Default Configs")
 
-    if alg_options["apply_selections"] == 1:
+    if options_model["find_filter_selections"]["value"] == 1:
         # 2. Run the Hill-climbing algorithm to find the best "image config combination"
         # filter_space.ignore_candidates.append(filter_space.best_candidate.position)
         sgt_hill_climbing_algorithm(filter_space, ntwk_obj.image_obj, max_iters=max_iters)
         _print_configs("Selected Configs")
 
-    if alg_options["filter_values"] == 1:
+    if options_model["find_filter_values"]["value"] == 1:
         # 3. Run the Genetic Algorithm to find the best "image filter values"
         val_search_space = filter_space.best_candidate.value_space
         val_img_configs = sgt_genetic_algorithm(val_search_space, ntwk_obj.image_obj, generations=max_iters, pop_size=ga_init_pop)
@@ -413,7 +420,7 @@ def metaheuristic_image_configs(ntwk_obj: ImageProcessor, max_iters: int = 4, ga
         filter_space.img_configs = val_img_configs
         _print_configs("Best Image Configs")
 
-    if alg_options["brightness_contrast"] == 1:
+    if options_model["find_brightness_contrast"]["value"] == 1:
         # 4. Run the Genetic Algorithm to find the best "brightness/contrast values" (only if 'val_search_space' fxn fails)
         bright_search_space = filter_space.best_candidate.brightness_space
         brt_img_configs = sgt_genetic_algorithm(bright_search_space, ntwk_obj.image_obj, generations=max_iters, pop_size=ga_init_pop)
