c03f9d17460a6fcaedf76a27d2b26b8aea975737
owuordickson <owuordickson@gmail.com>
2025-10-29 22:55:47 -0400

    v3.7.0: power-law with exponential cutoff

--- src/sgtlib/__init__.py
+++ src/sgtlib/__init__.py
@@ -24,7 +24,7 @@ of the GNU General Public License along with this program.  If not, see <https:/
 
 
 # Project Details
-__version__ = "3.6.8"
+__version__ = "3.6.9"
 __install_version__ = "3.6.8"
 __title__ = "StructuralGT"
 __author__ = "Dickson Owuor"
