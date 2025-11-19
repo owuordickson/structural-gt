0361dc33c3e8cd9eb153e235cce2d828cb32ab75
owuordickson <owuordickson@gmail.com>
2025-11-19 11:33:45 -0500

    v3.7.2: updates

--- src/sgtlib/compute/graph_analyzer.py
+++ src/sgtlib/compute/graph_analyzer.py
@@ -882,7 +882,7 @@ class GraphAnalyzer(ProgressUpdate):
             num_patches = int(opt_gtc["scaling_behavior_patches_per_kernel"]["value"])
 
             parts.append(f"Kernel Count = {num_filters}")
-            parts.append(f"No. of Random Locations={num_patches}")
+            parts.append(f"No. of Random Locations = {num_patches}")
 
         # Add title if needed
         if parts:
