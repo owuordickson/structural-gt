291990508e8259ae0624187ef531c6d177e2f5aa
Dickson Owuor (Ph.D.) <43271336+owuordickson@users.noreply.github.com>
2025-12-24 13:34:03 -0500

    Update main.py

--- results/scaling_laws_data_package/code/main.py
+++ results/scaling_laws_data_package/code/main.py
@@ -1,5 +1,5 @@
 """
-1. Make sure you install sgtlib v3.5.3 via: `pip install sgtlib==3.5.3`
+1. Make sure you install sgtlib v3.5.4 via: `pip install sgtlib==3.5.4`
 2. Run this script to re-generate the results we got.
 """
 
